d8f3916ade9f2bb62553a9e69f05d56c16c596d9
Matheus Conrado <dev.conrado@gmail.com>
2020-05-02 23:55:38 -0300

    added SignIn and SigUp pages with validations

---
+++ src/pages/SignUp/index.tsx
@@ -0,0 +1,60 @@
+import React, { useCallback, useRef } from 'react';
+import { FiMail, FiUser, FiLock, FiArrowLeft } from 'react-icons/fi';
+import { Form } from '@unform/web';
+import { FormHandles } from '@unform/core';
+import * as Yup from 'yup';
+import getValidationErrors from '../../utils/getValidationErrors';
+import { Container, Content, Background } from './styles';
+import logoGobarber from '../../assets/logo.svg';
+import Input from '../../components/Input';
+import Button from '../../components/Button';
+
+const SignUp: React.FC = () => {
+  const formRef = useRef<FormHandles>(null);
+
+  const handleSubmit = useCallback(async (data: object) => {
+    try {
+      formRef.current?.setErrors({});
+      const schema = Yup.object().shape({
+        name: Yup.string().required('Nome obrigatório'),
+        email: Yup.string()
+          .required('E-mail obrigatório')
+          .email('Digite um e-mail válido'),
+        password: Yup.string().min(6, 'Digite 6(seis) dígitos'),
+      });
+      await schema.validate(data, {
+        abortEarly: false,
+      });
+    } catch (error) {
+      const errors = getValidationErrors(error);
+      formRef.current?.setErrors(errors);
+      console.log(error);
+    }
+  }, []);
+
+  return (
+    <Container>
+      <Background />
+      <Content>
+        <Form ref={formRef} onSubmit={handleSubmit}>
+          <img src={logoGobarber} alt="Logo GoBarber" />
+          <h1>Faça seu Cadastro</h1>
+          <Input name="name" icon={FiUser} placeholder="Nome" />
+          <Input name="email" icon={FiMail} placeholder="E-mail" />
+          <Input
+            name="password"
+            icon={FiLock}
+            type="password"
+            placeholder="Senha"
+          />
+          <Button type="submit">Cadastrar</Button>
+        </Form>
+        <a href="SignUp">
+          <FiArrowLeft />
+          Volta para o Logon
+        </a>
+      </Content>
+    </Container>
+  );
+};
+export default SignUp;
